c5aacb0dee5302db62d9b8611bf84dcfedabb1a3
Roman <AverkievRE@yandex.ru>
2022-06-15 22:16:21 +0300

    Реализация проэкта Task_2

---
+++ Task_2/Program.cs
@@ -0,0 +1,27 @@
+﻿/*
+ * Выведите на экран надпись Hello World!!!, 
+ * используя метод Console.Write. 
+   Используйте Console.Write для каждого слова 
+   (у вас будет три вызова метода: сначала для слова Hello, 
+   потом для слова World, а затем — для трёх восклицательных знаков).
+   Чтобы слова не «слипались», используйте пробелы внутри кавычек.
+   Добавьте задержку Console.ReadLine, 
+   чтобы приложение не закрывалось по завершении выполнения.
+ */
+
+using System;
+
+namespace Task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Hello ");
+            Console.Write("World ");
+            Console.Write("!!!");
+
+            Console.ReadLine();
+        }
+    }
+}
