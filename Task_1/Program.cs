a4c5ed6a61763fb7856d789192b6c1ac02e07a08
Roman <AverkievRE@yandex.ru>
2022-06-15 22:11:25 +0300

    Реализован проект Task_1

---
+++ Task_1/Program.cs
@@ -0,0 +1,21 @@
+﻿/*
+   Добавьте оператор вывода на экран Console.WriteLine
+   и с его помощью выведите надпись: Hello World!!!
+   Добавьте задержку Console.ReadKey, 
+   чтобы приложение не закрывалось по завершении выполнения.
+*/
+
+using System;
+
+namespace Task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!!!");
+
+            Console.ReadKey();
+        }
+    }
+}
